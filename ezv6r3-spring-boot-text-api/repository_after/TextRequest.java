b258d73e0c3eb64da2080dcedc840ee795a61576
ekram <ekruiwc2020@gmail.com>
2026-01-26 14:04:12 +0300

    chore(83F1T8): initial project structure

--- ezv6r3-spring-boot-text-api/repository_after/TextRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.textapi;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record TextRequest(
-    @NotBlank(message = "Text must not be null, empty, or whitespace only")
-    String text
-) {}
