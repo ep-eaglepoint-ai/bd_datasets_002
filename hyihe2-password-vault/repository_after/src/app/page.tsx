3dcb1effbe7ddb4adafece7d143acd6543b8e753
million-art <millionmulugeta09@gmail.com>
2026-01-27 14:55:38 +0300

    feat: Create the home page rendering `VaultGuard` and `VaultDashboard` components.

--- hyihe2-password-vault/repository_after/src/app/page.tsx
+++ hyihe2-password-vault/repository_after/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { VaultDashboard } from "@/components/vault/VaultDashboard";
 import { VaultGuard } from "@/components/vault/VaultGuard";
-
+// home page
 export default function Home() {
   return (
     <VaultGuard>
