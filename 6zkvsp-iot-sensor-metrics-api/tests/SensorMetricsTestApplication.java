1548427e99e29ed4dee30ad4e1e04f34cfdac66f
Dagim Tesfaye <dagimcodes@gmail.com>
2026-01-28 14:34:40 +0300

    test(6zkvsp): added initial tests

---
+++ 6zkvsp-iot-sensor-metrics-api/tests/SensorMetricsTestApplication.java
@@ -0,0 +1,11 @@
+package com.example.sensormetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorMetricsTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SensorMetricsTestApplication.class, args);
+    }
+}
