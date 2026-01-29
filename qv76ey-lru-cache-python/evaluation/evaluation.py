fec1ac50f2fc09b2d6537ff91fbe329c9a8f385c
temesgen-abebayehu <temesgensoftware21@gmail.com>
2026-01-29 23:22:15 +0300

    fix typo and import

--- qv76ey-lru-cache-python/evaluation/evaluation.py
+++ qv76ey-lru-cache-python/evaluation/evaluation.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 import uuid
@@ -29,7 +30,7 @@ def run_tests(repo_path: Path):
             capture_output=True,
             text=True,
             timeout=120,
-            env={**subprocess.os.environ, **env}
+            env={**os.environ, **env}
         )
         
         return {
