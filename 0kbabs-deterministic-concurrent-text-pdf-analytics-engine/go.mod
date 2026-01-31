563b51a0ff9e7020e43b4e7784ef1088235a835d
million-art <millionmulugeta09@gmail.com>
2026-01-31 17:58:39 +0300

    Remove mistakenly added folders from other branches

--- 0kbabs-deterministic-concurrent-text-pdf-analytics-engine/go.mod
+++
@@ -1,3 +0,0 @@
-module analyzer
-
-go 1.24.3
--- epyqas-typescript-authentication/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/repository_after/src/$1',
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testMatch: ['<rootDir>/tests/**/*.test.ts'],
-};
