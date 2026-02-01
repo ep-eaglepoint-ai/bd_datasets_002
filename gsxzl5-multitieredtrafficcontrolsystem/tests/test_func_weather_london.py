44435becb251c16ff5f1f7e3b6df670aec0927aa
Hailu <hailegebrelyalemberabate@outlook.com>
2026-02-01 08:49:49 +0300

    fixed folder structure

--- gsxzl5-multitieredtrafficcontrolsystem/tests/test_func_weather_london.py
+++
@@ -1,9 +0,0 @@
-
-from api_server import APIServer
-
-def test_func_weather_london():
-    server = APIServer()
-    req = {'path': '/weather', 'ip': '1.1.1.1', 'user_id': None, 'payload': {}}
-    res = server.handle_request(req)
-    assert res['status'] == 200
-    assert res['data']['city'] == "London"
